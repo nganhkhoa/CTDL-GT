716e15b6c84d4121dbb7259afd80762e66bf0b7b
Luibo <ng.akhoa@yahoo.com.vn>
2017-09-28 08:02:44 +0700

    tut4lab

---
+++ include/tut4lab.cpp
@@ -0,0 +1,11 @@
+#include <tut4lab.h>
+
+namespace week4
+{
+namespace lab
+{}    // namespace lab
+namespace tut
+{}    // namespace tut
+namespace onclass
+{}    // namespace onclass
+}
---
+++ include/tut4lab.h
@@ -0,0 +1,14 @@
+#ifndef TUT4LAB_H
+#define TUT4LAB_H
+
+namespace week4
+{
+namespace lab
+{}    // namespace lab
+namespace tut
+{}    // namespace tut
+namespace onclass
+{}    // namespace onclass
+}
+
+#endif
